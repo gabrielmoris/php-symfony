f6671f31b8fb498db1b390c38cea2cdaeff19668
gabriel <gabriel.moris@ameropa.de>
2023-08-24 09:23:13 +0200

    lesson 28

---
+++ public/readme.txt
@@ -0,0 +1,16 @@
+Thank you for using our template!
+
+For more awesome templates please visit https://colorlib.com/wp/templates/
+
+
+Copyright information for the template can't be altered/removed unless you purchase a license.
+More information about the license is available here: https://colorlib.com/wp/licence/
+
+Removing copyright information without the license will result in suspension of your hosting and/or domain name(s).
+
+
+Das Template wurde im Rahmen des Udemy Kurses verändert und erweitert. 
+
+Leider hat Colorlib das Lizenzierungsmodell mittlerweile geändert. Zum Zeitpunkt der Erstellung konnte das Template kostenlose heruntergeladenen werden. Es steht aber nach wie vor unter CC BY 3.0. (Sie dürfen: Teilen - das Material in jedwedem Format oder Medium vervielfältigen und weiterverbreiten. Bearbeiten - das Material remixen, verändern und darauf aufbauen und zwar für beliebige Zwecke, sogar kommerziell. Der Lizenzgeber kann diese Freiheiten nicht widerrufen solange Sie sich an die Lizenzbedingungen halten. Namensnennung - Sie müssen angemessene Urheber- und Rechteangaben machen, einen Link zur Lizenz beifügen und angeben, ob Änderungen vorgenommen wurden.)
+
+https://creativecommons.org/licenses/by/3.0/de/
---
+++ src/Controller/GerichtController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Gericht;
+use App\Form\GerichtType;
+use App\Repository\GerichtRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
+#[Route('/gericht', name: 'gericht.')]
+class GerichtController extends AbstractController
+{
+    #[Route('/', name: 'bearbeiten')]
+    public function index(GerichtRepository $gr): Response
+    {
+        $gerichte = $gr->findAll();
+
+        return $this->render('gericht/index.html.twig', [
+            'gerichte' => $gerichte,
+        ]);
+    }
+
+    #[Route('/anlegen', name: 'anlegen')]
+    public function anlegen(ManagerRegistry $doctrine, Request $request): Response
+    {
+       $gericht = new Gericht();
+       $form = $this->createForm(GerichtType::class,$gericht);
+       $form->handleRequest($request);
+
+       if($form->isSubmitted()){
+            //EntityManager
+            $em = $doctrine->getManager();
+            $bild = $request->files->get('gericht')['anhang'];
+
+            if($bild){
+                $dateiname=md5(uniqid()). '.' .$bild-> guessClientExtension();
+            }
+
+            $bild->move(
+                $this->getParameter('bilder_ordner'),
+                $dateiname
+            );
+
+            $gericht->setBild($dateiname);
+            $em->persist($gericht);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('gericht.bearbeiten'));
+       }
+
+   
+
+       // response
+       return $this->render('gericht/anlegen.html.twig', [
+        'anlegenForm' => $form ->createView(),
+    ]);
+    }
+
+    #[Route('/entfernen/{id}', name: 'entfernen')]
+    public function entfernen(ManagerRegistry $doctrine, $id, GerichtRepository $gr): Response
+    {
+        $em = $doctrine->getManager();
+        $gericht = $gr->find($id);
+        $em->remove($gericht);
+        $em->flush();
+        // mesage
+        $this->addFlash('erfolg','Gericht wurde erfolgreich entfernt');
+        return $this->redirect($this->generateUrl('gericht.bearbeiten'));
+    }
+
+    #[Route('/anzeigen/{id}', name: 'anzeigen')]
+    public function anzeigen(Gericht $gericht){
+        return $this->render('gericht/anzeigen.html.twig', [
+            'gericht' => $gericht,
+        ]);
+    }
+}
