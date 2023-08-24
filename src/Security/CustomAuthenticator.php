b395fcc5863935d00c177b4460a7df85756233ca
gabriel <gabriel.moris@ameropa.de>
2023-08-24 15:32:24 +0200

    add rights to the routes

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -47,7 +47,7 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('gericht.bearbeiten'));
     }
 
     protected function getLoginUrl(Request $request): string
